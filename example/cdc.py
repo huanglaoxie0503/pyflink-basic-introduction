996e2ac89708df5d6025f2c148df069cf4870721
黄和帅 <huangheshuai@mingxinsk.com>
2023-07-29 11:26:01 +0800

    PyFlink 实现Flink CDC 实时同步MySQL数据到 Doris 案例

--- example/cdc.py
+++ example/cdc.py
@@ -4,6 +4,56 @@ from pyflink.table import EnvironmentSettings, TableEnvironment
 from utils.common import get_jar_file
 
 
+def mysql_cdc_2_doris(env):
+    """
+    Flink CDC 实时同步 MySQL 数据到 Doris 案例
+    """
+    source_mysql = """
+            CREATE TABLE cdc_mysql_source (
+                id integer,
+                name STRING,
+                PRIMARY key(id) not enforced
+            ) WITH (
+                'connector' = 'mysql-cdc',
+                'hostname' = '127.0.0.1',
+                'server-id' = '9057',
+                'port' = '3306',
+                'username' = 'root',
+                'password' = 'Oscar&0503',
+                'database-name' = 'flink',
+                'table-name' = 'source_table'
+            );
+        """
+    # -- 支持同步insert/update/delete事件
+    sink_to_doris = """
+    CREATE TABLE doris_sink (
+    id INT,
+    name STRING
+    ) 
+    WITH (
+      'connector' = 'doris',
+      'fenodes' = '119.91.147.68:8031',
+      'table.identifier' = 'ods.source_table',
+      'username' = 'root',
+      'password' = '',
+      'sink.properties.format' = 'json',
+      'sink.properties.read_json_by_line' = 'true',
+      'sink.enable-delete' = 'true',  -- 同步删除事件
+      'sink.label-prefix' = 'doris_label'
+    );
+    """
+
+    insert_sql = "insert into doris_sink select id,name from cdc_mysql_source;"
+
+    env.execute_sql(source_mysql)
+    env.execute_sql(sink_to_doris)
+
+    statement_set = env.create_statement_set()
+    statement_set.add_insert_sql(insert_sql)
+
+    statement_set.execute().wait()
+
+
 def mysql_cdc_2_mysql(env):
     """
     Flink CDC 实时同步 MySQL 数据到 MySQL案例
@@ -51,17 +101,37 @@ def mysql_cdc_2_mysql(env):
             'driver' = 'com.mysql.cj.jdbc.Driver'
         )
     """
+    sink_to_doris = """
+        CREATE TABLE doris_sink (
+        id INT,
+        name STRING
+        ) 
+        WITH (
+          'connector' = 'doris',
+          'fenodes' = '119.91.147.68:8031',
+          'table.identifier' = 'ods.source_table',
+          'username' = 'oscar',
+          'password' = 'Oscar@0503',
+          'sink.properties.format' = 'json',
+          'sink.properties.read_json_by_line' = 'true',
+          'sink.enable-delete' = 'true',  -- 同步删除事件
+          'sink.label-prefix' = 'doris_label'
+        );
+        """
 
     insert_sql_print = "INSERT INTO sink_print2 SELECT * FROM source_table"
-    insert_sql_table = "INSERT INTO sink_table SELECT * FROM source_table"
+    insert_sql_mysql = "INSERT INTO sink_table SELECT * FROM source_table"
+    insert_sql_doris = "insert into doris_sink select id,name from source_table"
 
     env.execute_sql(source_mysql)
     env.execute_sql(sink_print)
     env.execute_sql(sink_to_mysql)
+    env.execute_sql(sink_to_doris)
 
     statement_set = env.create_statement_set()
     statement_set.add_insert_sql(insert_sql_print)
-    statement_set.add_insert_sql(insert_sql_table)
+    statement_set.add_insert_sql(insert_sql_mysql)
+    # statement_set.add_insert_sql(insert_sql_doris)
 
     statement_set.execute().wait()
 
@@ -73,8 +143,9 @@ if __name__ == '__main__':
 
     path = '/Users/oscar/software/jars'
     filters = ['flink-connector-jdbc-3.1.0-1.17.jar', 'flink-sql-connector-mysql-cdc-2.4.1.jar',
-               'mysql-connector-j-8.0.33.jar']
+               'mysql-connector-j-8.0.33.jar', 'flink-doris-connector-1.17-1.4.0.jar']
     str_jars = get_jar_file(dir_path=path, need_jars=filters)
     t_env.get_config().set("pipeline.jars", str_jars)
 
     mysql_cdc_2_mysql(env=t_env)
+    mysql_cdc_2_doris(env=t_env)
